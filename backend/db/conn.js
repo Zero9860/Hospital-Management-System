0d314ccc22245eeb507ea08ef9e0ff9b7bdf9d94
Zero9860 <aashishthapaliya89@gmail.com>
2024-06-18 12:18:33 +0545

    database uurl

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,8 +1,7 @@
 
 import mongoose from  'mongoose'
-const url = "mongodb+srv://Suresh_11:sx8QPFwYoNEX5BSg@cluster0.ivodyjl.mongodb.net/hospital"
 
-// const url = "mongodb://localhost:27017/hospitalRegistraion "
+const url = "mongodb://localhost:27017/hospitalRegistraion "
 
 export const db= ()=>{ 
     mongoose.connect(url).then(() => {
